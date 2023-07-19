90cb83de80ba2bf21fb01d8dce5bf834542b858e
Hemanth-Sappa <134211683+Hemanth-Sappa@users.noreply.github.com>
2023-07-19 22:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let div = document.createElement("div");
 div.id = "browser-info";
-div.innertext = "You are using " + browserName + " version " + version;
+div.innertext = '"You are using " + browserName + " version " + version';
 document.body.appendChild(div);
